ee39761a49b81baa76d4a1870999c813bff2ba6d
jackylu <jackylu@ly.com>
2019-06-26 20:08:27 +0800

    修改项目并提交到git本地仓库

--- GIT/src/main/java/cn/lucan/GitTest.java
+++ GIT/src/main/java/cn/lucan/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("idea .....");
     }
+
+    public void show(){
+        System.out.println("show...");
+    }
 }
